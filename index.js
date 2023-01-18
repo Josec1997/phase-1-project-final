3681456bdc3116b247fcee109102ad9dc35904f1
Jose Campos <Jose_cmps14@yahoo.com>
2023-01-17 23:29:41 -0600

    tuck form away event

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ const addBoot = false
 document.addEventListener('DOMContentLoaded', () => {
     const addBtn = document.querySelector('#new-boots-btn')
     const bootsContainer = document.getElementsByClassName('boots-container')
-    
+
+    //tucks form away untill clicked
+    addBtn.addEventListener('click', () => {
+        addBoot= !addBoot
+        if (addBoot) {
+            bootsContainer.style.display = 'block'
+        } else {
+            bootsContainer.style.display = 'none'
+            }
+        }
+    )
 })
+
+
+    //get all data of boots 
+    fetch('')
