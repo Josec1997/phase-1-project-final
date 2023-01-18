8b5c9fa85af48bdcfffaebb91aa979cd579d9e4e
Jose Campos <Jose_cmps14@yahoo.com>
2023-01-18 00:01:08 -0600

    edit error

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //get all data of boots JSON
     fetch('http://localhost:3000/boots')
         .then(resp => resp.json())
-        .then (boot => forEach (boot => {
-            createBootCard(boot)
+        .then (bootData => bootData.forEach (bootObj => {
+            createBootCard(bootObj)
         }))
+
+
