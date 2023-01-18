7d08c2959b11b853f873608ed50af32594a1255e
Jose Campos <Jose_cmps14@yahoo.com>
2023-01-17 23:20:40 -0600

    added variables to js file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const addBoot = false 
 
 document.addEventListener('DOMContentLoaded', () => {
+    const addBtn = document.querySelector('#new-boots-btn')
+    const bootsContainer = document.getElementsByClassName('boots-container')
     
 })
