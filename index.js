142d13f77b605d60d12bae54de1eb5c47c6a48b4
Jose Campos <Jose_cmps14@yahoo.com>
2023-01-18 20:36:12 -0600

    add bootobj

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 
 
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (getBoots()) => {
 
+})
 
     //get all data of boots JSON
-    fetch('http://localhost:3000/boots')
+    function getBoots() {
+        fetch('http://localhost:3000/boots')
         .then(resp => resp.json())
         .then (bootData => bootData.forEach (bootObj => {
             createBootCard(bootObj)
         }))
+    }
 
+    function createBootCard(bootObj) {
+        let div = document.createElement('div')
+        div.className = 'card'
 
+        let h3 = document.createElement('h3')
+        h3.innerText= bootObj.name 
+
+        let img = document.createElement('img')
+        img.src = bootObj.img
+        img.className = 'boots-avatar'
+
+        let p = document.createElement('p')
+        p.textContent = 
+
+    }
 
