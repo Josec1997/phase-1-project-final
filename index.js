ec7e40c8c2c0eaa54f3ff439a36b972f00b3d252
Jose Campos <Jose_cmps14@yahoo.com>
2023-01-23 00:22:09 -0600

    updated project errors

--- index.js
+++ index.js
@@ -8,9 +8,11 @@ document.addEventListener('DOMContentLoaded',getBoots)
             createBootCard(bootObj)))
         }
 
+        
     function createBootCard(boots) {
         let div = document.createElement('div')
-            div.className = 'card'
+            div.className = `${boots.name}`
+            div.id = boots.id
 
         let h3 = document.createElement('h3')
             h3.innerText= boots.name 
@@ -27,49 +29,63 @@ document.addEventListener('DOMContentLoaded',getBoots)
              removeBtn.className= 'remove-button'
              removeBtn.id = boots.id
              removeBtn.textContent = 'remove'
-             deleteBoots(removeBtn)
+             removeBtn.addEventListener('click',(e) => {
+                deleteBoots(e)
+            const deleteCard = document.getElementById(`${boots.id}`)
+              deleteCard.remove()
+              alert(`${boots.name} Got the Boot!`)
+             })
              div.append(removeBtn)
 
-         let bootCollection = document.querySelector('.boot-collection')
+
+         let bootCollection = document.querySelector('#boot-collection')
             div.append(h3,img,p,removeBtn)
              bootCollection.append(div)
     }
 
-    function deleteBoots(){
-        addEventListener('click',() => 
-        fetch(`http://localhost:3000/boots/${boots.id}`,{
+    function deleteBoots(e){ 
+        fetch(`http://localhost:3000/boots/${e.target.id}`,{
             method: 'DELETE',
             headers: {
                 'Content-Type':'application/json'
             },
-            body:JSON.stringify(boots)
+            body:JSON.stringify()
         })
         .then(r => r.json())
-        .then(r => console.log(r)) 
-     )}
+        .then(r =>console.log(r))
+     }
 
         let submitBootObj = document.querySelector('form')
-            submitBootObj.addEventListener('submit', createNewBootObj)
-            function createNewBootObj(e) {
-                e.preventDefault()
+            submitBootObj.addEventListener('submit', submitNewBootObj)
+            // function createNewBootObj(e) {
+                // e.preventDefault()
                 
-            let newBootObj = {
-                'name':e.target.name.value,
-                'color':e.target.color.value,
-                'image':e.target.image.value,
-                //delete .value
-            }
-            submitNewBootObj(newBootObj)
-        }
+        //     let newBootObj = {
+        //         'name':e.target.name.value,
+        //         'color':e.target.color.value,
+        //         'image':e.target.image.value,
+                
+        //     }
+        //     submitNewBootObj(newBootObj)
+        // }
 
         function submitNewBootObj(newBootObj) {
-            fetch(`http://localhost:3000/boots/${boots.id}`, {
+            newBootObj.preventDefault()
+            let name = newBootObj.target.name.value
+            let color = newBootObj.target.color.value
+            let img = newBootObj.target.image.value
+
+            fetch(`http://localhost:3000/boots`, {
                 method: 'POST' ,
                 headers:{
                     'Content-Type': 'application/json',
                     Accept:'application/json' ,
                 },
-                body:JSON.stringify(newBootObj)
+                body:JSON.stringify({
+                    name:`${name}`,
+                    color:`${color}`,
+                    image:`${img}`, 
+                })
             })
             .then(r => r.json())
             .then((bootData) => createBootCard(bootData))
